e9e1214f3120d27d4d9f74b2c6e6fe0b0d70af50
Lemo0nD4y <mrpanther20@gmail.com>
2024-11-26 01:34:53 +0800

    resolving something

--- uptime_checker.py
+++ uptime_checker.py
@@ -1,16 +1,12 @@
 import requests
-import schedule
-import time
 from dotenv import load_dotenv
 import os
 
 # Load konfigurasi dari .env
 load_dotenv()
 
-
 # Daftar website yang akan dipantau
 websites = ["https://www.google.com", "https://anu123.com"]
-
 
 # Fungsi untuk memeriksa status website
 def check_website():
@@ -21,15 +17,10 @@ def check_website():
                 print(f"{website} is UP.")
             else:
                 print(f"{website} is DOWN. Status code: {response.status_code}")
-                
         except requests.exceptions.RequestException as e:
             print(f"{website} is DOWN. Error: {e}")
-           
 
-# Jadwalkan pengecekan setiap 5 menit
-schedule.every(1).minutes.do(check_website)
-
+# Jalankan pengecekan satu kali
 print("Website Uptime Checker is running...")
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+check_website()
+print("Website Uptime Checker has finished.")
